a2f44ec54bb9fd2fdf06efefc4fd086048efa5d1
Mohit Dubey <86982446+Mohit-3196@users.noreply.github.com>
2023-07-24 14:19:36 +0530

    Update create_app.sh
    Updated the script to remove -u and -p fields to pass username and password

--- create_app.sh
+++ create_app.sh
@@ -5,23 +5,18 @@
 # Function to display usage instructions
 usage() {
   echo "Usage:"
-  echo "$0 -n APPLICATION_NAME [-p INSTALLATION_PATH] [-w true|false] [-c true|false] [-m MONGO_HOST] [-u MONGO_USER] [-s MONGO_PASS] [-f WAR_FILE] [-r REDIS_HOST]"
+  echo "$0 -n APPLICATION_NAME [-p INSTALLATION_PATH] [-w true|false] [-h DATABASE_HOST] [-f WAR_FILE]"
   echo "Options:"
   echo "-n: Name of the application that you want to create. (Mandatory)"
   echo "-p: Path is the installation location of Ant Media Server. Default: /usr/local/antmedia"
   echo "-w: Flag to deploy the application as a war file. Default: false"
-  echo "-c: Flag to deploy the application in cluster mode. Default: false"
-  echo "-m: MongoDB host. Mandatory if in cluster mode, optional otherwise"
-  echo "-u: MongoDB user. Mandatory if in cluster mode, optional otherwise"
-  echo "-s: MongoDB password. Mandatory if in cluster mode, optional otherwise"
+  echo "-h: Database host with optional username and password (e.g., mongodb://username:password@host:port)."
   echo "-f: War file path for custom app deployment"
-  echo "-r: Redis host. Mandatory if in cluster mode, optional otherwise"
-  echo "-pu: MongoDB port. Default: 27017"
-  echo "-ps: Redis port. Default: 6379"
-  echo "-h: Print this usage"
+  echo "-q: Print this usage"
   echo " "
   echo "Example: "
   echo "$0 -n live -w"
+  echo "$0 -n live -h mongodb://localhost:27017"
   echo " "
   echo "If you have any questions, send an email to contact@antmedia.io"
 }
@@ -33,25 +28,17 @@ ERROR_MESSAGE="Error: Application creation failed. Please check the error in the
 
 AMS_DIR=/usr/local/antmedia
 AS_WAR=false
-IS_CLUSTER=false
 MONGO_PORT=27017
-REDIS_PORT=6379
 
 # Parse command-line options
-while getopts 'n:p:w:h:c:m:u:s:f:r:pu:ps:' option; do
+while getopts 'n:p:w:h:f:q' option; do
   case "${option}" in
     n) APP_NAME=${OPTARG} ;;
     p) AMS_DIR=${OPTARG} ;;
     w) AS_WAR=${OPTARG} ;;
-    c) IS_CLUSTER=${OPTARG} ;;
-    m) MONGO_HOST=${OPTARG} ;;
-    u) MONGO_USER=${OPTARG} ;;
-    s) MONGO_PASS=${OPTARG} ;;
+    h) DATABASE_HOST=${OPTARG} ;;
     f) WAR_FILE=${OPTARG} ;;
-    r) REDIS_HOST=${OPTARG} ;;
-    pu) MONGO_PORT=${OPTARG} ;;
-    ps) REDIS_PORT=${OPTARG} ;;
-    h) usage
+    q) usage
        exit 1 ;;
   esac
 done
@@ -82,15 +69,6 @@ if [[ -z "$AS_WAR" ]]; then
   AS_WAR=false
 fi
 
-# Validate MongoDB cluster mode parameters if specified
-if [[ "$IS_CLUSTER" == "true" ]]; then
-  if [[ -z "$MONGO_HOST" ]]; then
-    echo "Please set MongoDB host, username, and password for cluster mode."
-    usage
-    exit 1
-  fi
-fi
-
 # Format the AMS_DIR path
 case $AMS_DIR in
   /*) AMS_DIR=$AMS_DIR ;;
@@ -103,26 +81,20 @@ RED5_PROPERTIES_FILE=$APP_DIR/WEB-INF/red5-web.properties
 WEB_XML_FILE=$APP_DIR/WEB-INF/web.xml
 
 # Create the application directory
-mkdir $APP_DIR
-#check_result
+mkdir -p $APP_DIR
 
 # Copy the WAR file to the application directory
-echo $AMS_DIR
 cp $WAR_FILE $APP_DIR
-#check_result
 
 cd $APP_DIR
-#check_result
 
 WAR_FILE_NAME=$(basename $WAR_FILE)
 
 # Unzip the WAR file
 unzip $WAR_FILE_NAME
-#check_result
 
 # Remove the WAR file
 rm $WAR_FILE_NAME
-#check_result
 
 OS_NAME=$(uname)
 
@@ -130,18 +102,26 @@ if [[ "$OS_NAME" == 'Darwin' ]]; then
   SED_COMPATIBILITY='.bak'
 fi
 
-# Update MongoDB configuration in red5-web.properties
-if [[ -n "$MONGO_HOST" ]]; then
-  sed -i $SED_COMPATIBILITY 's#db.type=.*#db.type=mongodb#' $RED5_PROPERTIES_FILE
-  sed -i $SED_COMPATIBILITY 's#db.host=.*#db.host='$MONGO_HOST':'$MONGO_PORT'#' $RED5_PROPERTIES_FILE
-  sed -i $SED_COMPATIBILITY 's#db.user=.*#db.user='$MONGO_USER'#' $RED5_PROPERTIES_FILE
-  sed -i $SED_COMPATIBILITY 's#db.password=.*#db.password='$MONGO_PASS'#' $RED5_PROPERTIES_FILE
-fi
-
-# Update Redis configuration in red5-web.properties
-if [[ -n "$REDIS_HOST" ]]; then
-  sed -i $SED_COMPATIBILITY 's#db.type=.*#db.type=redis#' $RED5_PROPERTIES_FILE
-  sed -i $SED_COMPATIBILITY 's#db.host=.*#db.host='$REDIS_HOST':'$REDIS_PORT'#' $RED5_PROPERTIES_FILE
+# Update database configuration in red5-web.properties
+if [[ -n "$DATABASE_HOST" ]]; then
+  # Extract database type, username, password, host, and port from DATABASE_HOST
+  DATABASE_TYPE=$(echo $DATABASE_HOST | cut -d ':' -f 1)
+  HOST_PORT=$(echo $DATABASE_HOST | cut -d '@' -f 2)
+  if [[ "$DATABASE_TYPE" == "mongodb" ]]; then
+    # Extract username, password, host, and port for MongoDB
+    MONGO_USERNAME=$(echo $HOST_PORT | cut -d ':' -f 1 | cut -d '/' -f 3)
+    MONGO_PASSWORD=$(echo $MONGO_USERNAME | cut -d ':' -f 2)
+    MONGO_HOST=$(echo $HOST_PORT | cut -d '@' -f 2 | cut -d ':' -f 1)
+    MONGO_PORT=$(echo $HOST_PORT | cut -d '@' -f 2 | cut -d ':' -f 2)
+    if [ -n "$MONGO_USERNAME" ]; then
+      # If MongoDB username is provided, add it to the connection string
+      MONGO_CONNECTION="mongodb://$MONGO_USERNAME:$MONGO_PASSWORD@$MONGO_HOST:$MONGO_PORT"
+    else
+      MONGO_CONNECTION="mongodb://$MONGO_HOST:$MONGO_PORT"
+    fi
+    sed -i $SED_COMPATIBILITY 's#db.type=.*#db.type=mongodb#' $RED5_PROPERTIES_FILE
+    sed -i $SED_COMPATIBILITY 's#db.host=.*#db.host='$MONGO_CONNECTION'#' $RED5_PROPERTIES_FILE
+  fi
 fi
 
 # Update other properties in red5-web.properties
@@ -153,11 +133,6 @@ sed -i $SED_COMPATIBILITY 's#db.name=.*#db.name='$APP_NAME_LOWER'#' $RED5_PROPER
 # Update display name and context path in web.xml
 sed -i $SED_COMPATIBILITY 's#<display-name>StreamApp#<display-name>'$APP_NAME'#' $WEB_XML_FILE
 sed -i $SED_COMPATIBILITY 's#<param-value>/StreamApp#<param-value>/'$APP_NAME'#' $WEB_XML_FILE
-
-# Create a symbolic link for cluster mode
-if [[ "$IS_CLUSTER" == "true" ]]; then
-  ln -s $WAR_FILE $AMS_DIR/webapps/root/$APP_NAME.war
-fi
 
 # Perform additional operations based on AS_WAR flag
 if [[ $AS_WAR == "true" ]]; then
